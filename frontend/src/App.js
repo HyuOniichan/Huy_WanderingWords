b02a3eb2692ed0d353d424f77ed54da1288e6d73
Duc Huy <chuahetheky@gmail.com>
2024-10-30 22:39:02 +0700

    [fix] npm i wrong dir

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/baseComponents/Navbar";
 import HomeView from "./views/HomeView";
+import BlogView from './views/BlogView';
 
 function App() {
-  return (
-    <div className="App">
-		<HomeView />
-    </div>
-  )
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Routes>
+						<Route path='/' element={<HomeView />} />
+						<Route path='/blog' element={<BlogView />} />
+					</Routes>
+				</div>
+			</div>
+		</BrowserRouter>
+	)
 }
 
 export default App;
---
+++ frontend/src/views/BlogView.js
@@ -0,0 +1,9 @@
+function BlogView() {
+    return (
+        <div>
+            This is Blog view 
+        </div>
+    )
+}
+
+export default BlogView; 
