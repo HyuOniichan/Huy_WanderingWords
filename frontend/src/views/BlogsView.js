6f12073e39381d62a30812627860572d29f07ef6
Duc Huy <chuahetheky@gmail.com>
2024-11-01 09:51:24 +0700

    [fix] remove breadcrumb

--- frontend/src/views/BlogsView.js
+++ frontend/src/views/BlogsView.js
@@ -1,10 +1,8 @@
 import BlogList from "../components/blogComponents/BlogsComponents/BlogList";
-import Breadcrumb from "../components/baseComponents/Breadcrumb";
 
 function BlogsView() {
     return (
         <div>
-            <Breadcrumb />
             <BlogList /> 
         </div>
     )
