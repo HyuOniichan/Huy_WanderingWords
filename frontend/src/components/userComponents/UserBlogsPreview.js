59a49d643ecc18f474dda83e47f90e4d0d09fb63
Duc Huy <chuahetheky@gmail.com>
2024-11-03 11:20:08 +0700

    [feat](#34) function buttons user blogs

--- frontend/src/components/userComponents/UserBlogsPreview.js
+++ frontend/src/components/userComponents/UserBlogsPreview.js
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { UserContext } from "../../App";
 
 function UserBlogsPreview({ blog, setShowPreview }) {
+
+    const { username } = useParams();
+    const currentUser = useContext(UserContext);
+    const isMe = (currentUser[0].username === username); 
+
+    function deleteBlog(blogId) {
+        console.log(`${blogId} deleted`)
+    }
 
     return (
         <div className="col-6">
@@ -9,11 +19,18 @@ function UserBlogsPreview({ blog, setShowPreview }) {
                 <div className="card-body">
                     <h5 className="card-title">{blog ? blog.title : 'Title...'}</h5>
                     {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                    <Link to={blog ? `/blog/${blog._id}` : '#blogs'} className="btn btn-primary mt-2">Explore</Link>
+                    <div className="mt-3 mx-2 d-flex justify-content-between gap-2">
+                        <Link to={blog ? `/blog/${blog._id}` : '#blogs'} className="btn btn-primary w-100">
+                            {isMe? 'Edit' : 'Explore'}
+                        </Link>
+                        {isMe? <button className="btn btn-danger w-100" onClick={() => deleteBlog(blog._id)}>
+                            Delete
+                        </button> : ``}
+                    </div>
                 </div>
-                <button 
-                    type="button" 
-                    className="btn-close position-absolute top-0 end-0 m-1 bg-dark-subtle" 
+                <button
+                    type="button"
+                    className="btn-close position-absolute top-0 end-0 m-1 bg-dark-subtle"
                     aria-label="Close"
                     onClick={() => setShowPreview(null)}
                 ></button>
