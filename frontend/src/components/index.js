b860847274bc8ace4e19313be428afacf1659070
Duc Huy <chuahetheky@gmail.com>
2024-11-03 09:28:53 +0700

    [refactor](#20) change import link

---
+++ frontend/src/components/index.js
@@ -0,0 +1,33 @@
+// base components 
+import BackBtn from './baseComponents/BackBtn'
+
+// Home components
+
+// Blog components
+import BlogCard from './blogComponents/BlogsComponents/BlogCard'
+import BlogList from './blogComponents/BlogsComponents/BlogList'
+import BlogCreateEdit from './blogComponents/BlogCreateComponents/BlogCreateEdit'
+import BlogCreateOption from './blogComponents/BlogCreateComponents/BlogCreateOption'
+import BlogDetailPost from './blogComponents/BlogDetailComponents/BlogDetailPost'
+import BlogDetailRelated from './blogComponents/BlogDetailComponents/BlogDetailRelated'
+
+// User components
+import UserBlogsCard from './userComponents/UserBlogsCard'
+import UserBlogsPreview from './userComponents/UserBlogsPreview'
+import UserNavTabs from './userComponents/UserNavTabs'
+import UserProfileCard from './userComponents/UserProfileCard'
+
+// Export 
+export { 
+    BackBtn, 
+    BlogCard, 
+    BlogList, 
+    BlogCreateEdit, 
+    BlogCreateOption, 
+    BlogDetailPost, 
+    BlogDetailRelated, 
+    UserBlogsCard, 
+    UserBlogsPreview, 
+    UserNavTabs, 
+    UserProfileCard 
+}; 
--- frontend/src/views/UserView.js
+++ frontend/src/views/UserView.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import UserProfileCard from "../components/userComponents/UserProfileCard";
-import UserProfileScrollspy from "../components/userComponents/UserNavTabs";
-import UserBlogsCard from "../components/userComponents/UserBlogsCard";
+import { UserProfileCard, UserNavTabs, UserBlogsCard } from "../components/";
 
 function UserView() {
 
@@ -19,9 +17,9 @@ function UserView() {
 
     return (
         <div className="position-relative px-5" data-bs-spy="scroll" data-bs-target="#userTabs" data-bs-smooth-scroll="true" tabIndex="0">
-            <UserProfileScrollspy />
-            <UserProfileCard profile={profile} /> 
-            <UserBlogsCard profile={profile} />  
+            <UserNavTabs />
+            <UserProfileCard profile={profile} />
+            <UserBlogsCard profile={profile} />
         </div>
     )
 }
